0abe6f126374107a0fcdb7cc474b4067a21fd508
Bernat Jara Ortínez <bernat.jara@estudiantat.upc.edu>
2022-09-26 13:36:00 +0200

    Primeres probes

---
+++ src/LaMevaClase.java
@@ -0,0 +1,20 @@
+public class LaMevaClase implements Queue{
+    public LaMevaClase(int i) {
+    }
+
+    @Override
+    public void push(Object o) throws FullQueueException {
+        //
+        v[p++] = be;
+    }
+
+    @Override
+    public Object pop() throws EmptyQueueException {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
